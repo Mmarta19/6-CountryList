d9c06b8d9c237caa2d49ff9d3726e740b3cc47bf
Marta <martabarrachina1@hotmail.com>
2017-10-13 11:03:36 +0200

    Hem creat un Toast que ens diu quin país hem escollit

--- app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
@@ -2,8 +2,11 @@ package edu.upc.eseiaat.pma.countrylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,9 +28,25 @@ public class CountryListActivity extends AppCompatActivity {
         //Todos los ListView tienen un adaptador --> hace intermediario de la "Escalera Mecanica"
         adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, country_list);
         // 1. contexto: puntero a l'actividad. 2. Layout de cada pastillita ( cada item de la lista  para que tengan el mismo tamaño. 3. Datos
-
         list.setAdapter(adapter);
-        
+       //Cuando se clica en un Item de la lista
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View item, int pos, long id) {
+                Toast.makeText(
+                        CountryListActivity.this,
+                        String.format("Has escogido '%s'", country_list.get(pos)),
+                        Toast.LENGTH_SHORT
+                ).show();
+
+
+
+            }
+        });
+
+
+
+
 
 
 
